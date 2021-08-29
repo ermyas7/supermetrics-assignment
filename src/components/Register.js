2b679381b97379c424b3031c85b1a4f8818268f9
Ermyas Fekadu <ermyas.fekadu@gmail.com>
2021-08-29 17:20:19 +0300

    Add submit handler

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,19 +1,18 @@
 import { useForm } from '../hooks';
 
 const Register = () => {
-    const { inputs, changeHandler } = useForm();
-    console.log(inputs);
+    const { inputs, changeHandler, submitHandler } = useForm({name: '', email: ''});
     return(
         <div className="supermetrics-register">
             <h2 className="supermetrics-heading supermetrics-heading-secondary">Register Token</h2>
-            <form className="supermetrics-form">
+            <form className="supermetrics-form" onSubmit={submitHandler}>
                 <div className="supermetrics-input-group">
                     <label htmlFor="email">Email:</label>
-                    <input type="text" id="email" name="email" onChange={changeHandler}/>
+                    <input type="text" id="email" name="email" onChange={changeHandler} value={inputs.email}/>
                 </div>
                 <div className="supermetrics-input-group">
                     <label htmlFor="name">Name:</label>
-                    <input type="text" id="name" name="name" onChange={changeHandler}/>
+                    <input type="text" id="name" name="name" onChange={changeHandler} value={inputs.name}/>
                 </div>
                 <button type="submit">Register</button>
             </form>
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -5,11 +5,23 @@ export const useForm = (initialData = {}) => {
 
     const changeHandler = (evt) => {
         const { value, name } = evt.target;
-        setInputs({...inputs, name: value});
+        setInputs({...inputs, [name]: value});
     }
+
+    const submitHandler = (evt, cb = () => {}) => {
+        evt.preventDefault();
+        cb();
+        clearForm();
+    }
+
+    const clearForm = () => {
+        const clearedInputs = Object.fromEntries(Object.entries(inputs).map(([key, value]) => [key, '']));
+        setInputs(clearedInputs);
+    };
 
     return {
         inputs,
-        changeHandler
+        changeHandler,
+        submitHandler
     }
 };
